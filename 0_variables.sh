02ea91e353c343ac4e8776e554d0715b914dfff3
Zakariah Music <zakvmusic@gmail.com>
2021-06-27 02:39:35 -0400

    Squashed commit of the following:
    commit 37f679148929d476a44d9ff86d9ddf59aec67a19
    Author: ZakariahMusic <zakvmusic@gmail.com>
    Date:   Sun Jun 27 01:02:42 2021 -0400
    
        Create .DS_Store
    
    commit c8d32bea2c329e96d71fe600e61a29d51cf39f4b
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Wed Apr 14 22:03:33 2021 -0300
    
        added libvkd3d-dev dep
    
    commit 5f8f97b2c48740f32db439677701b31d7393cf7c
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Wed Apr 14 19:45:32 2021 -0300
    
        added missing native deps
    
    commit 3cd7100c35c5304f7f9e1820dee940c080213674
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Wed Apr 14 16:50:49 2021 -0300
    
        skipping compilation deps, using native
    
    commit 1d60768e1c134cb74dec9eb24aca45500cc6f00a
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Apr 12 07:22:17 2021 -0300
    
        testing v6.5
    
    commit 9b668f53a538dde214e3a43a22777802c29f7b8c
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Sun Apr 11 22:30:19 2021 -0300
    
        github Actions: removed set-env
    
    commit e2df4e779b605f449c23a24baaf9ca3caa8cd91e
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Oct 26 21:28:55 2020 -0300
    
        wine-staging 5.17.2
    
    commit 0ec5196ac4b708a17eca62de13f6c0f855f2a5ea
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Oct 26 20:51:58 2020 -0300
    
        using default libjpeg-dev
    
    commit 8816a6d545893770a72e305e1249acad611b0a97
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Oct 26 20:26:20 2020 -0300
    
        debug1
    
    commit 831d329fe4fb60512bfe8f1ba3e32b45b1a4fda3
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Oct 26 18:28:27 2020 -0300
    
        back with 5.17 with timeout_infinite_fix.patch, build fix and jpeg62
    
    commit a17c4017472c03afd935a9492ab2d1a19f944130
    Author: DanielDevBR <dados.linux@gmail.com>
    Date:   Mon Sep 28 07:45:42 2020 -0300
    
        updated to wine-staging 5.18, removed patch that got included

--- 0_variables.sh
+++ 0_variables.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-export WINE_VERSION="5.18"
+export WINE_VERSION="6.5"
 export STAGING_VERSION="${WINE_VERSION}"
-export TKG_GIT_COMMIT="e379f968e8c8e693be4330bfa5ac03595e7ed1ce"
-export TKG_SRC_FILENAME="wine-tkg-v${WINE_VERSION}-src.tar.gz"
 
 export SDL2_VERSION="2.0.12"
 export FAUDIO_VERSION="20.08"
@@ -16,7 +14,7 @@ export CHROOT_MIRROR="http://archive.ubuntu.com/ubuntu/"
 # amd64 or i386
 export UBUNTU_ARCH=amd64
 
-# nehalem go up to sse4.2https://github.com/Frogging-Family/wine-tkg-git/releases/tag/5.18.r3.g9acfa3b8
+# nehalem go up to sse4.2
 export CFLAGS="-march=nehalem -O2 -pipe -ftree-vectorize -fno-stack-protector"
 export CXXFLAGS="${CFLAGS}"
 export LDFLAGS="-Wl,-O1,--sort-common,--as-needed"
